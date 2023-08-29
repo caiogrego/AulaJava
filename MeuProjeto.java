e0da4808595c13c099331239843feb933a02f6b5
Caio <caiogrego@gmail.com>
2023-08-28 21:16:19 -0400

    Aula do dia 28.

--- MeuProjeto.java
+++ MeuProjeto.java
@@ -1,21 +1,33 @@
 
   public class MeuProjeto {
-    
+    static String nome;
     public int idade = 36;
     public double peso = 87.5;
     public char letra = 'a';
+
+    static void meuMetodo(String par) {
+      System.out.println(par);
+    }
+
     public static void main(String[] args) {
         MeuProjeto esteProjeto = new MeuProjeto();
         esteProjeto.idade = 34;
         esteProjeto.letra = 'c';
         esteProjeto.peso = 3.56;
         String texto = "Valor: ";
+        String MeuNovoNome = "Caio";
+        MeuProjeto.setNome(MeuNovoNome);
+        meuMetodo(MeuProjeto.getNome());
         System.out.println(texto + esteProjeto.peso);
         meuMetodo("" + esteProjeto.idade + esteProjeto.peso);
         meuMetodo(esteProjeto.idade + esteProjeto.peso + "");
     }
 
-    static void meuMetodo(String par) {
-      System.out.println(par);
+    public static String getNome() {
+      return MeuProjeto.nome;
     }
+    public static void setNome(String novoNome) {
+        MeuProjeto.nome = novoNome;
+    }
+
  }
